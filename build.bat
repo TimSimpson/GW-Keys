b5ee0ab88ce950761ce0732f334834bca990ab8a
Tim Simpson <timsimpson4@gmail.com>
2011-03-18 18:45:31 -0500

    Fixed typo in build.bat.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 msbuild /property:Configuration=Release /target:build
 copy /y GwKeysGui\bin\Release\GwKeysGui.exe Release\GwKeysGui.exe
-copy /y GwKeysGui\bin\Release\GwKeysGui.pdf Release\GwKeysGui.pdb
+copy /y GwKeysGui\bin\Release\GwKeysGui.pdb Release\GwKeysGui.pdb
